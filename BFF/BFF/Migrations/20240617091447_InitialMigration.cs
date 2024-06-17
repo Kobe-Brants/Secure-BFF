4079867e1e6bc8e2beffd72fa8677759ccfdbfc7
Kobe Brants <kobe.brants@telenet.be>
2024-06-17 11:38:26 +0200

    refactor: Migration sqlite

---
+++ BFF/BFF/Migrations/20240617091447_InitialMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BFF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Sessions",
+                columns: table => new
+                {
+                    id = table.Column<string>(type: "string", nullable: false),
+                    ClientId = table.Column<string>(type: "TEXT", nullable: false),
+                    AuthorizationEndpoint = table.Column<string>(type: "TEXT", nullable: false),
+                    Scopes = table.Column<string>(type: "TEXT", nullable: false),
+                    CodeVerifier = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sessions", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Sessions");
+        }
+    }
+}
